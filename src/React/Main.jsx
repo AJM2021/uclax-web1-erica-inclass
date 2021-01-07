7adc5f9fa6b42d0ecc8e9875a7844447d7a8781c
Erica Howard <ericahoward@Ericas-MacBook-Pro.local>
2021-01-06 19:43:25 -0800

    My first commit

---
+++ src/React/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* Components ---------------------------*/
+//import Test from './Test.jsx';
+
+const Main = () => {
+
+    return (
+        <div className='Main'>
+            <h1>Erica's In Class</h1>
+        </div>
+    );
+}
+
+export default Main;
