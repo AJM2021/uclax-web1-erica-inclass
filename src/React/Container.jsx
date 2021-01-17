49b14b23002ae500fe89efbb6f0f8b2e4fc01326
Erica Howard <ericahoward@Ericas-MacBook-Pro.local>
2021-01-17 15:24:24 -0800

    Updated from class on 21.01.13

---
+++ src/React/Container.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Nav from './Nav.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import SunAndMoon from './SunAndMoon.jsx'
+
+
+const Container = () => {
+    return (
+        <div className='Container'>
+            CONTENT
+            <Header />
+            <Nav />
+            <SunAndMoon />
+            <Footer />
+        </div>
+    );
+    }
+    export default Container; 
--- src/React/SunAndMoon.jsx
+++ src/React/SunAndMoon.jsx
@@ -15,6 +15,7 @@ const imageOut = () => {
         <div>
             The Sun and The Moon
             <img src={ imageSrc } 
+            alt='The Sun and Moon'
             onMouseOver={ imageOver }
             onMouseOut={ imageOut}
             
