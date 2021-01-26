01d2936b3cf8fe79aa30d29dac1e4168bc2aef33
Erica Howard <ericahoward@Ericas-MacBook-Pro.local>
2021-01-25 20:22:07 -0800

    Scaffolding our website.

--- src/React/Nav.jsx
+++ src/React/Nav.jsx
@@ -1,15 +1,15 @@
 
 import React from 'react';
 import styled from 'styled-components';
-
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <NavStyled className='Nav'>
-            <a href="/">Welcome</a>
-            <a href="/services">Services</a>
-            <a href="/contact">Contact</a>
-            <a href="/login">Login</a>
+            <NavLink to="/" exact>Welcome</NavLink>
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/login">Login</NavLink>
         </NavStyled>
     );
 }
@@ -24,6 +24,10 @@ a {
 
     padding: 10px;
     margin: 10px;
+
+    &.active {
+        background-color: #015150;
+    }
 
 }
 `;
